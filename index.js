8d06aab36e43338a0f7ffb53865b353bad81be1d
013Ratchanon <013Ratchanon@webmail.npru.ac.th>
2024-02-28 16:15:37 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2");
+
+const app = express();
+
+app.use(cors());
+
+const connection = mysql.createConnection(process.env.DATABASE_URL);
+
+app.get("/", (req, res) => {
+    console.log("Hello express");
+    res.send("Hello World");
+});
+
+app.get("/menufood", (req, res) => {
+    connection.query("SELECT foodID,foodName,foodDescription,foodPrice,tbl_menu.menuName FROM tbl_food , tbl_menu  WHERE tbl_food.MenuID = tbl_menu.MenuID", function (err, results, fields) {
+        res.send(results);
+    });
+});
+
+app.listen(process.env.PORT || 3000);
